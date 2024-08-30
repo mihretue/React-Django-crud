e37c23da6b11c1a3f5b0377f31e08352136a63bd
Mihretu Endeshaw <106491338+mihretue@users.noreply.github.com>
2024-08-30 22:32:09 +0300

    some changes to the django app

--- Client/book-store/src/App.jsx
+++ Client/book-store/src/App.jsx
@@ -2,16 +2,20 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [ book, setBook] = useState([])
+  const [ books, setBook] = useState([])
+  const [title, setTitle]= useState("")
+  const [releaseYear, setReleaseYear] = useState(0)
+
+
   useEffect(
     () => {
-      fetchBook()
+      fetchBooks()
       },[]
   )
-  const fetchBook = async ()=>{
+  const fetchBooks = async ()=>{
     try{
       const response = await fetch('http://127.0.0.1:8000/api/book/')
-      const data = response.json()
+      const data = await response.json()
       setBook(data)
     } catch(err){
       console.log(err)
@@ -19,23 +23,49 @@ function App() {
 
   }
 
+
+const createBook = async (e)=>{
+  e.preventDefault()
+  //creating object for the input data
+  const bookData ={
+    title:title,
+    received_date:releaseYear
+  }
+  
+        const response = await fetch('http://127.0.0.1:8000/api/book/create/',
+          {
+            method: 'POST',
+            headers:{
+              'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(bookData)
+          }
+        )
+        if(response.ok){
+          const data = await response.json()
+          console.log(data)
+
+          //to authomatically fetching the created data
+          setBook((prev)=>[...prev, data])
+        }
+        else{
+          console.log("Error")
+          }
+}
   return (
     <>
       <div>
-        <input type='text' placeholder='Book Title'/>
-        <input typep='text' placeholder='Book Received Date'/>
-        <button >Submit</button>
+        <input type='text' placeholder='Book Title'  onChange={(e)=>setTitle(e.target.value)} />
+        <input typep='text' placeholder='Book Received Date' onChange={(e)=>setReleaseYear(e.target.value)}/>
+        <button onClick={createBook}>Submit</button>
       </div>
-      <div>
-        {book.map(()=>{
-          return (<>
-            <div >{book.title}</div>
-            <div>{book.received_date}</div>
-            </>
-          )
-
-        })}
-      </div>
+      
+        {books.map((book)=>(  
+          <div>
+            <p >{book.title}</p>
+            <p>{book.received_date}</p>
+          </div>
+        ))}
     </>
   )
 }
